9f8b2bce007dc12cca73481f2acfa428d0e31a21
JavierLuisG <javierandresluisgonzalez@gmail.com>
2024-01-16 14:54:37 -0500

    Modificación del LWC ‘speciesTile’ para montar y desplegar en la Org

--- force-app/main/default/lwc/speciesTile/speciesTile.js
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -1,3 +1,12 @@
 import { LightningElement } from "lwc";
 
-export default class SpeciesTile extends LightningElement {}
+export default class SpeciesTile extends LightningElement {
+  // especie (Objeto -> variable de instancia o propiedad)
+  specie = {
+    Name: "Aloe",
+    Description: "Grandiosa para el cuidado de la piel",
+    Image_URL_c:
+      "https://www.shutterstock.com/es/image-photo/aloe-vera-sliced-gel-dripping-isolated-2162658879",
+    Location__c: "Outdoors"
+  };
+}
